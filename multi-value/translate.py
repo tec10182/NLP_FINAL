929a901b9415c6b6662eb4eb1448e7b502fd9b7a
Minsu Kange <tec10182@naver.com>
2025-05-29 15:50:02 +0900

    feat: RTE&SST VALUE train

--- multi-value/translate.py
+++ multi-value/translate.py
@@ -9,7 +9,7 @@ nig = NigerianDialect()
 ind = IndianDialect()
 
 # 데이터 로드
-df = pd.read_csv("GLUE/SST/train.tsv", sep='\t')
+df = pd.read_csv("GLUE/RTE/train.tsv", sep='\t')
 
 # 성공/에러 카운트 초기화
 success_count = 0
@@ -27,13 +27,15 @@ def safe_transform(text):
         return None
 
 # 문장 변환 적용
-df["sentence"] = df["sentence"].progress_apply(safe_transform)
+df["sentence1"] = df["sentence1"].progress_apply(safe_transform)
+df["sentence2"] = df["sentence2"].progress_apply(safe_transform)
 
 # None 값 있는 행 제거
-df = df.dropna(subset=["sentence"]).reset_index(drop=True)
+df = df.dropna(subset=["sentence1"]).reset_index(drop=True)
+df = df.dropna(subset=["sentence2"]).reset_index(drop=True)
 
 # 결과 저장
-res = "VALUE/SST/train_nigerian_transformed.tsv"
+res = "VALUE/RTE/train_nig_transformed.tsv"
 df.to_csv(res, sep="\t", index=False)
 
 # 처리 결과 출력
